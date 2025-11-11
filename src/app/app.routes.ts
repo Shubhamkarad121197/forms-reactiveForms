9873403aa36c4b02eb4ebced4c9a29e9f6328d34
shubham karad <shubham.karad12@gmail.com>
2025-11-12 01:20:35 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', loadComponent:()=>import('./components/template-driven/template-driven.component').then((m)=>m.TemplateDrivenComponent)},
+    {path:'ReactiveForms',loadComponent:()=>import('./components/reactive-forms/reactive-forms.component').then((m)=>m.ReactiveFormsComponent)}
+];
+
+
+
