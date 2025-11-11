43f669fa698158b8c0ecc59b1a9320b2c1d17267
shubham karad <shubham.karad12@gmail.com>
2025-11-12 01:34:55 +0530

    template Driven

--- src/app/components/template-driven/template-driven.component.ts
+++ src/app/components/template-driven/template-driven.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './template-driven.component.html',
   styleUrl: './template-driven.component.css'
 })
 export class TemplateDrivenComponent {
 
+  user={
+    email:'',
+    password:''
+  }
+
+   onSubmit(form: NgForm) {
+    console.log('Form submitted:', form.value);
+  }
 }
